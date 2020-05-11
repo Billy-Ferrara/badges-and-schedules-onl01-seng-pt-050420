773a3582394d53309fbf0b68330843bb8c74423e
Billy Ferrara <willfira717@gmail.com>
2020-05-11 18:30:18 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,6 +3,6 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-  attendees.each do |"Hello, my name is #{attendees}."|
-  end 
+  attendees.each do |{"Hello, my name is #{attendees}."}|
+
 end
