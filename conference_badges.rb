70724e3cac87a304087640873b88a05f4c177ce0
Billy Ferrara <willfira717@gmail.com>
2020-05-11 18:47:01 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,11 +11,11 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(array)
-  array = []
+  new_array = []
   counter = 1
-  array.each do |name|
-    array.push("Hello, #{name}! You'll be assigned to room #{counter}!")
+  new_array.each do |name|
+    new_array.push("Hello, #{name}! You'll be assigned to room #{counter}!")
     counter += 1
   end
-  return array
+  return new_array
 end
